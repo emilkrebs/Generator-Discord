700ecd421837be1a3941b9a2fcaec22552c4c9a5
emilkrebs <onlime@gmx.de>
2023-03-10 13:06:23 +0100

    Added args and command to all templates

--- templates/javascript/src/index.js
+++ templates/javascript/src/index.js
@@ -3,19 +3,35 @@ const { token } = require('../config.json');
 
 // Create a new client instance with Intents that specify which privileged intents your bot requires
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
+const prefix = '!';
 
 // Run this code when the client is ready. This event will only trigger one time after logging in
 client.once('ready', () => {
   console.log(`Bot is logged in as ${client.user.tag}.`);
 });
 
+
 // Listen for messages
 client.on('messageCreate', async message => {
-  const { content } = message;
-  // Reply with "Pong!" when someone says "!ping"
-  if (content === "!ping") {
-    return await message.reply("Pong!");
+  // Ignore messages that don't start with the prefix or are from a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  // Split the message into command and arguments
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift()?.toLowerCase();
+
+  // ping - bot will reply with Pong!
+  if (command === 'ping') {
+    await message.reply('Pong!');
+  }
+  // say <message> - bot will send the message
+  else if (command === 'say') {
+    if (!args.length) {
+      await message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+      return;
+    }
+    await message.channel.send(args.join(' '));
+    return;
   }
 });
 
@@ -23,4 +39,4 @@ client.on('messageCreate', async message => {
 // Login to Discord with your app's token
 client.login(token);
 
-// For more information about the Discord.js library, visit https://discord.js.org/#/docs/main/stable/general/welcome
+// For more information about the Discord.js library, visit https://discord.js.org/#/docs/main/stable/general/welcome∏
