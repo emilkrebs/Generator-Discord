bc7246e97a6ef007797bbc6248b1821bc3f1a44c
emilkrebs <emil.krebs@typefox.io>
2023-08-31 23:41:40 +0200

    removed spaces from templates

--- templates/javascript/src/index.js
+++ templates/javascript/src/index.js
@@ -7,32 +7,32 @@ const prefix = '!';
 
 // Run this code when the client is ready. This event will only trigger one time after logging in
 client.once('ready', () => {
-  console.log(`Bot is logged in as ${client.user.tag}.`);
+	console.log(`Bot is logged in as ${client.user.tag}.`);
 });
 
 
 // Listen for messages
 client.on('messageCreate', async message => {
-  // Ignore messages that don't start with the prefix or are from a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+	// Ignore messages that don't start with the prefix or are from a bot
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  // Split the message into command and arguments
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift()?.toLowerCase();
+	// Split the message into command and arguments
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift()?.toLowerCase();
 
-  // ping - bot will reply with Pong!
-  if (command === 'ping') {
-    await message.reply('Pong!');
-  }
-  // say <message> - bot will send the message
-  else if (command === 'say') {
-    if (!args.length) {
-      await message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-      return;
-    }
-    await message.channel.send(args.join(' '));
-    return;
-  }
+	// ping - bot will reply with Pong!
+	if (command === 'ping') {
+		await message.reply('Pong!');
+	}
+	// say <message> - bot will send the message
+	else if (command === 'say') {
+		if (!args.length) {
+			await message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+			return;
+		}
+		await message.channel.send(args.join(' '));
+		return;
+	}
 });
 
 
